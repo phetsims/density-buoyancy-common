7707c93c56ca99accb25dd3372b6095f7eb20cd7
Jonathan Olson <jonathan.olson@colorado.edu>
2019-04-24 03:34:29 -0600

    Matter.js initial tests

--- js/common/model/DemoModel.js
+++ js/common/model/DemoModel.js
@@ -7,6 +7,7 @@ define( require => {
   'use strict';
 
   // modules
+  const Bounds2 = require( 'DOT/Bounds2' );
   const densityBuoyancyCommon = require( 'DENSITY_BUOYANCY_COMMON/densityBuoyancyCommon' );
 
   /**
@@ -18,7 +19,40 @@ define( require => {
      * @param {Tandem} tandem
      */
     constructor( tandem ) {
-      //TODO
+
+      // @public {number}
+      this.leftX = -300;
+      this.rightX = 300;
+      this.groundY = 0;
+      this.poolY = -400;
+      this.farLeftX = -1000;
+      this.farRightX = 1000;
+      this.farBelow = -1000;
+
+      // @private {Matter.Engine}
+      this.engine = Matter.Engine.create();
+      this.engine.world.gravity.y = -1; // So that it's physical with positive y up
+
+      // @public {Array.<Matter.Body>}
+      this.groundBodies = [
+        DemoModel.staticBodyFromBounds( new Bounds2( this.farLeftX, this.farBelow, this.leftX, this.groundY ) ),
+        DemoModel.staticBodyFromBounds( new Bounds2( this.leftX, this.farBelow, this.rightX, this.poolY ) ),
+        DemoModel.staticBodyFromBounds( new Bounds2( this.rightX, this.farBelow, this.farRightX, this.groundY ) )
+      ];
+
+      // @public {Array.<Matter.Body>}
+      this.mobileBodies = [
+        DemoModel.bodyFromRectangle( 290, 50, 100, 100 )
+      ];
+      Matter.Body.setMass( this.mobileBodies[ 0 ], 20 );
+      Matter.Body.setInertia( this.mobileBodies[ 0 ], Number.POSITIVE_INFINITY );
+
+      // @public {Array.<Matter.Body>}
+      this.bodies = [ ...this.groundBodies, ...this.mobileBodies ];
+
+      Matter.World.add( this.engine.world, this.bodies );
+
+      // MouseConstraint (stiffness)
     }
 
     // @public resets the model
@@ -28,7 +62,25 @@ define( require => {
 
     // @public
     step( dt ) {
-      //TODO Handle model animation here.
+      Matter.Engine.update( this.engine, dt * 1000 );
+    }
+
+    static bodyFromRectangle( centerX, centerY, width, height ) {
+      return Matter.Bodies.rectangle( centerX, centerY, width, height );
+    }
+
+    static staticBodyFromBounds( bounds ) {
+      return Matter.Bodies.rectangle( bounds.centerX, bounds.centerY, bounds.width, bounds.height, {
+        isStatic: true
+      } );
+      // return Matter.Bodies.fromVertices( 0, 0, [
+      //   Matter.Vector.create( bounds.minX, bounds.minY ),
+      //   Matter.Vector.create( bounds.minX, bounds.maxY ),
+      //   Matter.Vector.create( bounds.maxX, bounds.maxY ),
+      //   Matter.Vector.create( bounds.maxX, bounds.minY )
+      // ], {
+      //   isStatic: true
+      // } );
     }
   }
 
--- js/common/view/DemoScreenView.js
+++ js/common/view/DemoScreenView.js
@@ -7,9 +7,15 @@ define( function( require ) {
   'use strict';
 
   // modules
+  const Bounds2 = require( 'DOT/Bounds2' );
   const densityBuoyancyCommon = require( 'DENSITY_BUOYANCY_COMMON/densityBuoyancyCommon' );
+  const ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
+  const Path = require( 'SCENERY/nodes/Path' );
+  const Rectangle = require( 'SCENERY/nodes/Rectangle' );
   const ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   const ScreenView = require( 'JOIST/ScreenView' );
+  const Shape = require( 'KITE/Shape' );
+  const Vector2 = require( 'DOT/Vector2' );
 
   class DemoScreenView extends ScreenView {
 
@@ -20,6 +26,34 @@ define( function( require ) {
     constructor( model, tandem ) {
 
       super();
+
+      const scale = 0.75;
+
+      // @private {DemoModel}
+      this.model = model;
+
+      // @private {ModelViewTransform2}
+      this.modelViewTransform = ModelViewTransform2.createSinglePointScaleInvertedYMapping( Vector2.ZERO, this.layoutBounds.center, scale );
+
+      const modelPoolShape = Shape.polygon( [
+        new Vector2( model.farLeftX, model.groundY ),
+        new Vector2( model.leftX, model.groundY ),
+        new Vector2( model.leftX, model.poolY ),
+        new Vector2( model.rightX, model.poolY ),
+        new Vector2( model.rightX, model.groundY ),
+        new Vector2( model.farRightX, model.groundY ),
+        new Vector2( model.farRightX, model.farBelow ),
+        new Vector2( model.farLeftX, model.farBelow )
+      ] );
+      const viewPoolShape = this.modelViewTransform.modelToViewShape( modelPoolShape );
+
+      this.mobile1 = Rectangle.bounds( new Bounds2( -50 * scale, -50 * scale, 50 * scale, 50 * scale ), { stroke: 'red' } );
+      this.addChild( this.mobile1 );
+      // TODO: init
+
+      this.addChild( new Path( viewPoolShape, {
+        stroke: 'black'
+      } ) );
 
       const resetAllButton = new ResetAllButton( {
         listener: () => {
@@ -34,6 +68,9 @@ define( function( require ) {
 
     // @public
     step( dt ) {
+      this.mobile1.rotation = -this.model.mobileBodies[ 0 ].angle;
+      const position = this.model.mobileBodies[ 0 ].position;
+      this.mobile1.translation = this.modelViewTransform.modelToViewPosition( new Vector2( position.x, position.y ) );
       //TODO Handle view animation here.
     }
   }

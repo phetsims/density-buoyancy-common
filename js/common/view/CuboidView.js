e9d4f56b5aec618011a7b8175238c5b82d1daa1d
Jonathan Olson <jonathan.olson@colorado.edu>
2019-06-13 12:36:19 -0600

    Improved dynamic mesh handling for Cuboid

--- js/common/view/CuboidView.js
+++ js/common/view/CuboidView.js
@@ -10,6 +10,9 @@ define( require => {
   const densityBuoyancyCommon = require( 'DENSITY_BUOYANCY_COMMON/densityBuoyancyCommon' );
   const MassView = require( 'DENSITY_BUOYANCY_COMMON/common/view/MassView' );
 
+  // constats
+  const bufferSize = 18 * 6;
+
   class CuboidView extends MassView {
     /**
      * @param {Cuboid} cuboid
@@ -17,12 +20,14 @@ define( require => {
     constructor( cuboid ) {
       const size = cuboid.sizeProperty.value;
 
-      const positionArray = CuboidView.getPositionArray( size );
-      const normalArray = CuboidView.getNormalArray();
+      const positionArray = new Float32Array( bufferSize );
+      const normalArray = new Float32Array( bufferSize );
+
+      CuboidView.updateArrays( positionArray, normalArray, size );
 
       const boxGeometry = new THREE.BufferGeometry();
-      boxGeometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positionArray ), 3 ) );
-      boxGeometry.addAttribute( 'normal', new THREE.BufferAttribute( new Float32Array( normalArray ), 3 ) );
+      boxGeometry.addAttribute( 'position', new THREE.BufferAttribute( positionArray, 3 ) );
+      boxGeometry.addAttribute( 'normal', new THREE.BufferAttribute( normalArray, 3 ) );
 
       super( cuboid, boxGeometry );
 
@@ -31,14 +36,11 @@ define( require => {
 
       // @private {function}
       this.sizeListener = size => {
-        const positionArray = CuboidView.getPositionArray( size );
-        for ( let i = 0; i < positionArray.length; i++ ) {
-          boxGeometry.attributes.position.array[ i ] = positionArray[ i ];
-        }
+        CuboidView.updateArrays( boxGeometry.attributes.position.array, null, size );
         boxGeometry.attributes.position.needsUpdate = true;
         boxGeometry.computeBoundingSphere();
       };
-      this.cuboid.sizeProperty.lazyLink( this.sizeListener );
+      this.cuboid.sizeProperty.link( this.sizeListener );
     }
 
     /**
@@ -52,20 +54,46 @@ define( require => {
       super.dispose();
     }
 
-    static getPositionArray( size ) {
-      const array = [];
-      function quad( p0x, p0y, p0z, p1x, p1y, p1z, p2x, p2y, p2z, p3x, p3y, p3z ) {
-        array.push(
-          p0x, p0y, p0z,
-          p1x, p1y, p1z,
-          p2x, p2y, p2z,
+    /**
+     * Updates provided geometry arrays given the specific size.
+     * @private
+     *
+     * @param {Float32Array|null} positionArray
+     * @param {Float32Array|null} normalArray
+     * @param {Bounds3} size
+     */
+    static updateArrays( positionArray, normalArray, size ) {
+      let positionIndex = 0;
+      let normalIndex = 0;
 
-          p0x, p0y, p0z,
-          p2x, p2y, p2z,
-          p3x, p3y, p3z
-        );
+      function position( x, y, z ) {
+        if ( positionArray ) {
+          positionArray[ positionIndex++ ] = x;
+          positionArray[ positionIndex++ ] = y;
+          positionArray[ positionIndex++ ] = z;
+        }
       }
 
+      function normal( x, y, z ) {
+        if ( normalArray ) {
+          for ( let i = 0; i < 6; i++ ) {
+            normalArray[ normalIndex++ ] = x;
+            normalArray[ normalIndex++ ] = y;
+            normalArray[ normalIndex++ ] = z;
+          }
+        }
+      }
+
+      function quad( p0x, p0y, p0z, p1x, p1y, p1z, p2x, p2y, p2z, p3x, p3y, p3z ) {
+        position( p0x, p0y, p0z );
+        position( p1x, p1y, p1z );
+        position( p2x, p2y, p2z );
+
+        position( p0x, p0y, p0z );
+        position( p2x, p2y, p2z );
+        position( p3x, p3y, p3z );
+      }
+      
       // Bottom
       quad(
         size.minX, size.minY, size.minZ,
@@ -73,6 +101,7 @@ define( require => {
         size.maxX, size.minY, size.maxZ,
         size.minX, size.minY, size.maxZ
       );
+      normal( 0, -1, 0 );
 
       // Top
       quad(
@@ -81,6 +110,7 @@ define( require => {
         size.maxX, size.maxY, size.maxZ,
         size.maxX, size.maxY, size.minZ
       );
+      normal( 0, 1, 0 );
 
       // Left
       quad(
@@ -89,6 +119,7 @@ define( require => {
         size.minX, size.maxY, size.maxZ,
         size.minX, size.maxY, size.minZ
       );
+      normal( -1, 0, 0 );
 
       // Right
       quad(
@@ -97,6 +128,7 @@ define( require => {
         size.maxX, size.maxY, size.maxZ,
         size.maxX, size.minY, size.maxZ
       );
+      normal( 1, 0, 0 );
 
       // Back
       quad(
@@ -105,6 +137,7 @@ define( require => {
         size.maxX, size.maxY, size.minZ,
         size.maxX, size.minY, size.minZ
       );
+      normal( 0, 0, -1 );
 
       // Front
       quad(
@@ -113,55 +146,7 @@ define( require => {
         size.maxX, size.maxY, size.maxZ,
         size.minX, size.maxY, size.maxZ
       );
-
-      return array;
-    }
-
-    static getNormalArray() {
-      // TODO: easier way to repeat these
-      return [
-        0, -1, 0,
-        0, -1, 0,
-        0, -1, 0,
-        0, -1, 0,
-        0, -1, 0,
-        0, -1, 0,
-
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-
-        -1, 0, 0,
-        -1, 0, 0,
-        -1, 0, 0,
-        -1, 0, 0,
-        -1, 0, 0,
-        -1, 0, 0,
-
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-
-        0, 0, -1,
-        0, 0, -1,
-        0, 0, -1,
-        0, 0, -1,
-        0, 0, -1,
-        0, 0, -1,
-
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1
-      ];
+      normal( 0, 0, 1 );
     }
   }
 

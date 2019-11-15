630c64660ffe801697820b8b63c75c2c497f36d6
Jonathan Olson <jonathan.olson@colorado.edu>
2019-11-15 13:46:53 -0700

    Interior material colors

--- js/buoyancy/view/BottleView.js
+++ js/buoyancy/view/BottleView.js
@@ -8,8 +8,11 @@ define( require => {
 
   // modules
   const Bottle = require( 'DENSITY_BUOYANCY_COMMON/buoyancy/model/Bottle' );
+  const Color = require( 'SCENERY/util/Color' );
   const densityBuoyancyCommon = require( 'DENSITY_BUOYANCY_COMMON/densityBuoyancyCommon' );
   const MassView = require( 'DENSITY_BUOYANCY_COMMON/common/view/MassView' );
+  const Material = require( 'DENSITY_BUOYANCY_COMMON/common/model/Material' );
+  const ThreeUtil = require( 'MOBIUS/ThreeUtil' );
 
   class BottleView extends MassView {
     /**
@@ -26,72 +29,62 @@ define( require => {
       const bottomClipPlane = new THREE.Plane( new THREE.Vector3( 0, -1, 0 ), 0 );
       const topClipPlane = new THREE.Plane( new THREE.Vector3( 0, 1, 0 ), 0 );
 
-      liquidYProperty.link( y => {
-        bottomClipPlane.constant = y;
-        topClipPlane.constant = -y;
+      const backTopMaterial = new THREE.MeshPhongMaterial( {
+        color: 0xffffff,
+        opacity: 0.4,
+        transparent: true,
+        side: THREE.BackSide,
+        depthWrite: false,
+        clippingPlanes: [ topClipPlane ]
       } );
+      const backTop = new THREE.Mesh( primaryGeometry, backTopMaterial );
+      this.add( backTop );
 
-      // this.add( new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
-      //   color: 0xcccccc,
-      //   clippingPlanes: [ bottomClipPlane ]
-      // } ) ) );
-
-      // this.add( new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
-      //   color: 0x888888,
-      //   clippingPlanes: [ topClipPlane ]
-      // } ) ) );
-
-      // Back top
-      this.add( new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
-        color: 0xffffff,
-        opacity: 0.4,
+      const backBottomMaterial = new THREE.MeshPhongMaterial( {
+        color: 0x33FF33,
+        opacity: 0.8,
         transparent: true,
         side: THREE.BackSide,
-        // depthWrite: false,
-        clippingPlanes: [ topClipPlane ]
-      } ), {
-        renderOrder: 1
-      } ) );
-
-      // Back bottom
-      this.add( new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
-        color: 0xffffff,
-        opacity: 0.4,
-        transparent: true,
-        side: THREE.BackSide,
-        // depthWrite: false,
+        depthWrite: false,
         clippingPlanes: [ bottomClipPlane ]
-      } ), {
-        renderOrder: 2
-      } ) );
+      } );
+      const backBottom = new THREE.Mesh( primaryGeometry, backBottomMaterial );
+      this.add( backBottom );
 
-      // Front top
-      this.add( new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
+      const frontTopMaterial = new THREE.MeshPhongMaterial( {
         color: 0xffffff,
         opacity: 0.4,
         transparent: true,
         side: THREE.FrontSide,
-        // depthWrite: false,
+        depthWrite: false,
         clippingPlanes: [ topClipPlane ]
-      } ), {
-        renderOrder: 3
-      } ) );
+      } );
+      const frontTop = new THREE.Mesh( primaryGeometry, frontTopMaterial );
+      this.add( frontTop );
 
-      // Front bottom
-      this.add( new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
-        color: 0xffffff,
-        opacity: 0.4,
+      const frontBottomMaterial = new THREE.MeshPhongMaterial( {
+        color: 0x33FF33,
+        opacity: 0.8,
         transparent: true,
         side: THREE.FrontSide,
-        // depthWrite: false,
+        depthWrite: false,
         clippingPlanes: [ bottomClipPlane ]
-      } ), {
-        renderOrder: 4
-      } ) );
+      } );
+      const frontBottom = new THREE.Mesh( primaryGeometry, frontBottomMaterial );
+      this.add( frontBottom );
 
-      this.add( new THREE.Mesh( Bottle.getCapGeometry(), new THREE.MeshLambertMaterial( {
+      const frontForDepth = new THREE.Mesh( primaryGeometry, new THREE.MeshPhongMaterial( {
+        color: 0xFF0000,
+        opacity: 0,
+        transparent: true,
+        side: THREE.FrontSide
+      } ) );
+      this.add( frontForDepth );
+
+      const cap = new THREE.Mesh( Bottle.getCapGeometry(), new THREE.MeshLambertMaterial( {
         color: 0xFF3333
-      } ) ) );
+      } ) );
+      this.add( cap );
 
       const crossSectionPositionArray = Bottle.createCrossSectionVertexArray();
       const crossSectionNormalArray = new Float32Array( crossSectionPositionArray.length );
@@ -113,11 +106,78 @@ define( require => {
         setCrossSectionRelativeY( Bottle.getYFromVolume( volume ) );
       } );
 
-      const interiorSurface = new THREE.Mesh( interiorSurfaceGeometry, new THREE.MeshLambertMaterial( {
-        color: 0x33FF33
-      } ) );
+      // TODO: unlink
+      const adjustClipPlanes = () => {
+        const modelY = bottle.matrix.translation.y + Bottle.getYFromVolume( bottle.interiorVolumeProperty.value );
+
+        bottomClipPlane.constant = modelY;
+        topClipPlane.constant = -modelY;
+      };
+      bottle.transformedEmitter.addListener( adjustClipPlanes );
+      bottle.interiorVolumeProperty.lazyLink( adjustClipPlanes );
+      adjustClipPlanes();
+
+      const interiorSurfaceMaterial = new THREE.MeshPhongMaterial( {
+        color: 0x33FF33,
+        opacity: 0.8,
+        transparent: true,
+        depthWrite: false,
+        side: THREE.DoubleSide
+      } );
+      const interiorSurface = new THREE.Mesh( interiorSurfaceGeometry, interiorSurfaceMaterial );
 
       this.add( interiorSurface );
+
+      backTop.renderOrder = -5;
+      backBottom.renderOrder = -4;
+      interiorSurface.renderOrder = -3;
+      frontTop.renderOrder = -2;
+      frontBottom.renderOrder = -1;
+
+      bottle.interiorMaterialProperty.link( material => {
+        let color = 0xffffff;
+        let opacity = 0.4;
+
+        if ( material === Material.WATER ) {
+          color = ThreeUtil.colorToThree( new Color( 0, 128, 255 ) );
+          opacity = 0.4;
+        }
+        else if ( material === Material.GASOLINE ) {
+          color = ThreeUtil.colorToThree( new Color( 230, 255, 0 ) );
+          opacity = 0.4;
+        }
+        else if ( material === Material.OIL ) {
+          color = ThreeUtil.colorToThree( new Color( 170, 255, 0 ) );
+          opacity = 0.6;
+        }
+        else if ( material === Material.SAND ) {
+          color = ThreeUtil.colorToThree( new Color( 194, 178, 128 ) );
+          opacity = 1;
+        }
+        else if ( material === Material.CEMENT ) {
+          color = ThreeUtil.colorToThree( new Color( 128, 130, 133 ) );
+          opacity = 1;
+        }
+        else if ( material === Material.COPPER ) {
+          color = ThreeUtil.colorToThree( new Color( 184, 115, 51 ) );
+          opacity = 1;
+        }
+        else if ( material === Material.LEAD ) {
+          color = ThreeUtil.colorToThree( new Color( 80, 85, 90 ) );
+          opacity = 1;
+        }
+        else if ( material === Material.MERCURY ) {
+          color = ThreeUtil.colorToThree( new Color( 219, 206, 202 ) );
+          opacity = 1;
+        }
+
+        interiorSurfaceMaterial.color = color;
+        backBottomMaterial.color = color;
+        frontBottomMaterial.color = color;
+        interiorSurfaceMaterial.opacity = opacity;
+        backBottomMaterial.opacity = opacity;
+        frontBottomMaterial.opacity = opacity;
+      } );
 
       // @public {Bottle}
       this.bottle = bottle;

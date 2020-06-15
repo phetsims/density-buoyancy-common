3f5fe30d4712a9a1a6a753c0b2bf38dcf69ff203
Jonathan Olson <jonathan.olson@colorado.edu>
2020-06-15 14:58:14 -0600

    Moving over Buoyancy intro to the modal variety

--- js/buoyancy/model/BuoyancyIntroModel.js
+++ js/buoyancy/model/BuoyancyIntroModel.js
@@ -6,11 +6,11 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import Property from '../../../../axon/js/Property.js';
 import Matrix3 from '../../../../dot/js/Matrix3.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Enumeration from '../../../../phet-core/js/Enumeration.js';
 import Cuboid from '../../common/model/Cuboid.js';
+import DensityBuoyancyModal from '../../common/model/DensityBuoyancyModal.js';
 import DensityBuoyancyModel from '../../common/model/DensityBuoyancyModel.js';
 import Material from '../../common/model/Material.js';
 import Scale from '../../common/model/Scale.js';
@@ -23,42 +23,53 @@ const Mode = Enumeration.byKeys( [
   'SAME_DENSITY'
 ] );
 
-class BuoyancyIntroModel extends DensityBuoyancyModel {
+class BuoyancyIntroModel extends DensityBuoyancyModal( DensityBuoyancyModel, Mode, Mode.SAME_MASS ) {
   /**
    * @param {Tandem} tandem
    */
   constructor( tandem ) {
+    const createMasses = ( model, mode ) => {
+      switch ( mode ) {
+        case Mode.SAME_MASS:
+          return [
+            Cuboid.createWithMass( model.engine, Material.WOOD, Vector2.ZERO, 5 ),
+            Cuboid.createWithMass( model.engine, Material.BRICK, Vector2.ZERO, 5 )
+          ];
+        case Mode.SAME_VOLUME:
+          return [
+            Cuboid.createWithVolume( model.engine, Material.WOOD, Vector2.ZERO, 0.005 ),
+            Cuboid.createWithVolume( model.engine, Material.BRICK, Vector2.ZERO, 0.005 )
+          ];
+        case Mode.SAME_DENSITY:
+          return [
+            Cuboid.createWithMass( model.engine, Material.WOOD, Vector2.ZERO, 2 ),
+            Cuboid.createWithMass( model.engine, Material.WOOD, Vector2.ZERO, 4 )
+          ];
+        default:
+          throw new Error( `unknown mode: ${mode}` );
+      }
+    };
 
-    super( tandem );
+    const positionMasses = ( model, mode, masses ) => {
+      switch ( mode ) {
+        case Mode.SAME_MASS:
+          model.positionMassesLeft( [ masses[ 0 ] ] );
+          model.positionMassesRight( [ masses[ 1 ] ] );
+          break;
+        case Mode.SAME_VOLUME:
+          model.positionMassesLeft( [ masses[ 0 ] ] );
+          model.positionMassesRight( [ masses[ 1 ] ] );
+          break;
+        case Mode.SAME_DENSITY:
+          model.positionMassesLeft( [ masses[ 0 ] ] );
+          model.positionMassesRight( [ masses[ 1 ] ] );
+          break;
+        default:
+          throw new Error( `unknown mode: ${mode}` );
+      }
+    };
 
-    // @public {Property.<Mode>}
-    this.modeProperty = new Property( Mode.SAME_MASS );
-
-    this.modeProperty.link( mode => {
-      this.setup();
-    } );
-  }
-
-  /**
-   * Sets up the scene with the given mode.
-   * @public
-   */
-  setup() {
-    this.clearMasses();
-
-    switch( this.modeProperty.value ) {
-      case Mode.SAME_MASS:
-        this.setupSameMass();
-        break;
-      case Mode.SAME_VOLUME:
-        this.setupSameVolume();
-        break;
-      case Mode.SAME_DENSITY:
-        this.setupSameDensity();
-        break;
-      default:
-        throw new Error( 'unknown mode: ' + this.modeProperty.value );
-    }
+    super( createMasses, positionMasses, tandem );
 
     // Left scale
     this.masses.push( new Scale( this.engine, {
@@ -73,78 +84,6 @@ class BuoyancyIntroModel extends DensityBuoyancyModel {
       displayType: Scale.DisplayType.NEWTONS,
       canMove: false
     } ) );
-  }
-
-  /**
-   * Sets up the initial state for the "Same Mass" mode.
-   * @public
-   */
-  setupSameMass() {
-    const masses = [
-      Cuboid.createWithMass( this.engine, Material.WOOD, Vector2.ZERO, 5 ),
-      Cuboid.createWithMass( this.engine, Material.BRICK, Vector2.ZERO, 5 )
-    ];
-
-    this.positionMassesLeft( [ masses[ 0 ] ] );
-    this.positionMassesRight( [ masses[ 1 ] ] );
-
-    this.masses.addAll( masses );
-  }
-
-  /**
-   * Sets up the initial state for the "Same Volume" mode.
-   * @public
-   */
-  setupSameVolume() {
-    const masses = [
-      Cuboid.createWithVolume( this.engine, Material.WOOD, Vector2.ZERO, 0.005 ),
-      Cuboid.createWithVolume( this.engine, Material.BRICK, Vector2.ZERO, 0.005 )
-    ];
-
-    this.positionMassesLeft( [ masses[ 0 ] ] );
-    this.positionMassesRight( [ masses[ 1 ] ] );
-
-    this.masses.addAll( masses );
-  }
-
-  /**
-   * Sets up the initial state for the "Same Density" mode.
-   * @public
-   */
-  setupSameDensity() {
-    const masses = [
-      Cuboid.createWithMass( this.engine, Material.WOOD, Vector2.ZERO, 2 ),
-      Cuboid.createWithMass( this.engine, Material.WOOD, Vector2.ZERO, 4 )
-    ];
-
-    this.positionMassesLeft( [ masses[ 0 ] ] );
-    this.positionMassesRight( [ masses[ 1 ] ] );
-
-    this.masses.addAll( masses );
-  }
-
-  /**
-   * Clears all of the masses away.
-   * @private
-   */
-  clearMasses() {
-    this.masses.forEach( mass => {
-      this.masses.remove( mass );
-      // TODO: memory management for the colors/etc.?
-    } );
-  }
-
-  /**
-   * Resets things to their original values.
-   * @public
-   * @override
-   */
-  reset() {
-    this.modeProperty.reset();
-
-    super.reset();
-
-    this.setup();
   }
 }
 

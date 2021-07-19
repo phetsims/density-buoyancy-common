67a7c90737b517f63d444e7c11bce6362bf25aec
phet-dev <phetadmin@gmail.com>
2021-07-19 03:34:25 -0600

    update copyright dates from daily grunt work

--- js/density/view/DensityReadoutNode.js
+++ js/density/view/DensityReadoutNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Displays a bar-scale with interactive density labels above/below and named reference values.

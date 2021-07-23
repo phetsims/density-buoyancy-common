9e04ed160bce2332059dd6bfc4abe0d82d5800a8
phet-dev <phetadmin@gmail.com>
2021-07-23 03:33:20 -0600

    update copyright dates from daily grunt work

--- js/common/view/ScaleReadoutNode.js
+++ js/common/view/ScaleReadoutNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Shows a readout in front of a scale, for its measured mass/weight.

6703af36f2c598358f2bdbbaa3def94d5198bfbd
Jonathan Olson <jonathan.olson@colorado.edu>
2021-08-23 08:39:16 -0600

    Adding phetioReadOnly to color properties, see https://github.com/phetsims/density-buoyancy-common/issues/65

--- js/common/view/DensityBuoyancyCommonColors.js
+++ js/common/view/DensityBuoyancyCommonColors.js
@@ -69,77 +69,92 @@ const DensityBuoyancyCommonColors = {
   comparingYellowColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'comparingYellow', {
     default: new Color( 252, 246, 80 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'comparingYellowColorProperty' )
   } ),
   comparingBlueColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'comparingBlue', {
     default: new Color( 46, 88, 166 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'comparingBlueColorProperty' )
   } ),
   comparingGreenColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'comparingGreen', {
     default: new Color( 125, 195, 52 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'comparingGreenColorProperty' )
   } ),
   comparingRedColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'comparingRed', {
     default: new Color( 233, 55, 50 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'comparingRedColorProperty' )
   } ),
   comparingPurpleColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'comparingPurple', {
     default: new Color( 131, 43, 126 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'comparingPurpleColorProperty' )
   } ),
 
   mysteryPinkColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryPink', {
     default: new Color( 255, 192, 203 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryPinkColorProperty' )
   } ),
   mysteryOrangeColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryOrange', {
     default: new Color( 255, 127, 0 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryOrangeColorProperty' )
   } ),
   mysteryLightPurpleColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryLightPurple', {
     default: new Color( 177, 156, 217 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryLightPurpleColorProperty' )
   } ),
   mysteryLightGreenColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryLightGreen', {
     default: new Color( 144, 238, 144 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryLightGreenColorProperty' )
   } ),
   mysteryBrownColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryBrown', {
     default: new Color( 150, 75, 0 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryBrownColorProperty' )
   } ),
   mysteryWhiteColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryWhite', {
     default: new Color( 255, 255, 255 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryWhiteColorProperty' )
   } ),
   mysteryGrayColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryGray', {
     default: new Color( 140, 140, 140 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryGrayColorProperty' )
   } ),
   mysteryMustardColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryMustard', {
     default: new Color( 225, 173, 0 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryMustardColorProperty' )
   } ),
   mysteryPeachColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryPeach', {
     default: new Color( 255, 229, 180 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryPeachColorProperty' )
   } ),
   mysteryMaroonColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'mysteryMaroon', {
     default: new Color( 128, 0, 0 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'mysteryMaroonColorProperty' )
   } ),
 
@@ -158,86 +173,103 @@ const DensityBuoyancyCommonColors = {
   materialAirColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialAir', {
     default: new Color( 0, 0, 0, 0 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialAirColorProperty' )
   } ),
   materialCementColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialCement', {
     default: new Color( 128, 130, 133 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialCementColorProperty' )
   } ),
   materialCopperColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialCopper', {
     default: new Color( 184, 115, 51 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialCopperColorProperty' )
   } ),
   materialDensityPColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialDensityP', {
     default: new Color( 255, 128, 128, 0.6 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialDensityPColorProperty' )
   } ),
   materialDensityQColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialDensityQ', {
     default: new Color( 128, 255, 128, 0.6 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialDensityQColorProperty' )
   } ),
   materialDensityRColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialDensityR', {
     default: new Color( 255, 128, 255, 0.6 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialDensityRColorProperty' )
   } ),
   materialDensitySColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialDensityS', {
     default: new Color( 128, 255, 255, 0.6 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialDensitySColorProperty' )
   } ),
   materialDensityXColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialDensityX', {
     default: new Color( 255, 255, 80, 0.6 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialDensityXColorProperty' )
   } ),
   materialDensityYColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialDensityY', {
     default: new Color( 80, 255, 255, 0.6 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialDensityYColorProperty' )
   } ),
   materialGasolineColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialGasoline', {
     default: new Color( 230, 255, 0, 0.4 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialGasolineColorProperty' )
   } ),
   materialHoneyColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialHoney', {
     default: new Color( 238, 170, 0, 0.8 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialHoneyColorProperty' )
   } ),
   materialLeadColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialLead', {
     default: new Color( 80, 85, 90 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialLeadColorProperty' )
   } ),
   materialMercuryColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialMercury', {
     default: new Color( 219, 206, 202 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialMercuryColorProperty' )
   } ),
   materialOilColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialOil', {
     default: new Color( 180, 230, 20, 0.4 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialOilColorProperty' )
   } ),
   materialSandColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialSand', {
     default: new Color( 194, 178, 128 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialSandColorProperty' )
   } ),
   materialSeawaterColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialSeawater', {
     default: new Color( 0, 150, 255, 0.4 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialSeawaterColorProperty' )
   } ),
   materialWaterColorProperty: new ProfileColorProperty( densityBuoyancyCommon, 'materialWater', {
     default: new Color( 0, 128, 255, 0.4 )
   }, {
+    phetioReadOnly: true,
     tandem: tandem.createTandem( 'materialWaterColorProperty' )
   } )
 };

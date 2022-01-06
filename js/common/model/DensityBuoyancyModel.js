fdae69ba47352c36e787c21787d2892211279524
phet-dev <phetadmin@gmail.com>
2022-01-06 03:15:38 -0700

    update copyright dates from daily grunt work

--- js/common/model/DensityBuoyancyModel.js
+++ js/common/model/DensityBuoyancyModel.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * The core model for the Density and Buoyancy sim screens, including a pool and masses.

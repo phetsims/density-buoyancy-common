739d32b464d503aa3d1003e5aa6eee903633898b
phet-dev <phetadmin@gmail.com>
2024-01-30 03:11:52 -0700

    update copyright dates from daily grunt work

--- js/common/model/Material.ts
+++ js/common/model/Material.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Represents different materials that solids/liquids in the simulations can take, including density/viscosity/color.

f293dd47bf81fe5d0e3931504f19720a4d5ed49b
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-16 15:47:45 -0600

    add TODO, https://github.com/phetsims/buoyancy/issues/139
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/common/model/Pool.ts
+++ js/common/model/Pool.ts
@@ -40,6 +40,7 @@ export default class Pool extends Basin {
    * liquid).
    */
   public isMassInside( mass: Mass ): boolean {
+    // TODO: Why not care about horizontal positioning? https://github.com/phetsims/buoyancy/issues/139
     return mass.stepBottom < this.stepTop;
   }
 

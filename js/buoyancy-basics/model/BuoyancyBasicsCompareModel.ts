a17aa385c1bd052e0879e19ca03b20f53ad117fe
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-25 15:15:52 -0600

    create screen scaffolding for buoyancy basics, https://github.com/phetsims/buoyancy-basics/issues/4 https://github.com/phetsims/buoyancy-basics/issues/5
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

---
+++ js/buoyancy-basics/model/BuoyancyBasicsCompareModel.ts
@@ -0,0 +1,92 @@
+// Copyright 2019-2024, University of Colorado Boulder
+
+/**
+ * The main model for the Compare screen of the Buoyancy: Basics simulation.
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import Matrix3 from '../../../../dot/js/Matrix3.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import Cube from '../../common/model/Cube.js';
+import DensityBuoyancyModel, { DensityBuoyancyModelOptions } from '../../common/model/DensityBuoyancyModel.js';
+import Material from '../../common/model/Material.js';
+import Scale, { DisplayType } from '../../common/model/Scale.js';
+import TwoBlockMode from '../../common/model/TwoBlockMode.js';
+import densityBuoyancyCommon from '../../densityBuoyancyCommon.js';
+import MassTag from '../../common/model/MassTag.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+
+type BuoyancyBasicsCompareModelOptions = DensityBuoyancyModelOptions;
+
+export default class BuoyancyBasicsCompareModel extends DensityBuoyancyModel {
+
+  public readonly modeProperty: Property<TwoBlockMode>;
+  public readonly primaryMass: Cube;
+  public readonly secondaryMass: Cube;
+  public readonly densityExpandedProperty: Property<boolean>;
+
+  public constructor( options: BuoyancyBasicsCompareModelOptions ) {
+
+    const tandem = options.tandem;
+
+    super( combineOptions<DensityBuoyancyModelOptions>( {
+      usePoolScale: true,
+      supportsDepthLines: true
+    }, options ) );
+
+    this.modeProperty = new EnumerationProperty( TwoBlockMode.ONE_BLOCK, {
+      tandem: tandem.createTandem( 'modeProperty' )
+    } );
+
+    const blocksTandem = tandem.createTandem( 'blocks' );
+
+    this.primaryMass = Cube.createWithMass( this.engine, Material.WOOD, new Vector2( -0.2, 0.2 ), 2, {
+      tag: MassTag.PRIMARY,
+      tandem: blocksTandem.createTandem( 'blockA' )
+    } );
+    this.availableMasses.push( this.primaryMass );
+    this.secondaryMass = Cube.createWithMass( this.engine, Material.ALUMINUM, new Vector2( 0.05, 0.35 ), 13.5, {
+      tag: MassTag.SECONDARY,
+      tandem: blocksTandem.createTandem( 'blockB' ),
+      visible: false
+    } );
+    this.availableMasses.push( this.secondaryMass );
+
+    this.modeProperty.link( mode => {
+      this.secondaryMass.internalVisibleProperty.value = mode === TwoBlockMode.TWO_BLOCKS;
+    } );
+
+    // Left scale
+    this.availableMasses.push( new Scale( this.engine, this.gravityProperty, {
+      matrix: Matrix3.translation( -0.65, -Scale.SCALE_BASE_BOUNDS.minY ),
+      displayType: DisplayType.NEWTONS,
+      tandem: tandem.createTandem( 'scale1' ),
+      canMove: true,
+      inputEnabledPropertyOptions: {
+        phetioReadOnly: false
+      }
+    } ) );
+
+    this.densityExpandedProperty = new BooleanProperty( false );
+  }
+
+  /**
+   * Resets things to their original values.
+   */
+  public override reset(): void {
+    this.modeProperty.reset();
+
+    this.primaryMass.reset();
+    this.secondaryMass.reset();
+
+    this.densityExpandedProperty.reset();
+
+    super.reset();
+  }
+}
+
+densityBuoyancyCommon.register( 'BuoyancyBasicsCompareModel', BuoyancyBasicsCompareModel );
---
+++ js/buoyancy-basics/view/BuoyancyBasicsExploreScreenView.ts
@@ -0,0 +1,182 @@
+// Copyright 2019-2024, University of Colorado Boulder
+
+/**
+ * The main view for the Explore screen of the Buoyancy: Basics simulation.
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import { AlignBox, ManualConstraint, Node, VBox } from '../../../../scenery/js/imports.js';
+import Panel from '../../../../sun/js/Panel.js';
+import DensityBuoyancyCommonConstants from '../../common/DensityBuoyancyCommonConstants.js';
+import Material from '../../common/model/Material.js';
+import DensityBuoyancyScreenView, { DensityBuoyancyScreenViewOptions } from '../../common/view/DensityBuoyancyScreenView.js';
+import FluidDensityControlNode from '../../common/view/FluidDensityControlNode.js';
+import PrimarySecondaryControlsNode from '../../common/view/PrimarySecondaryControlsNode.js';
+import densityBuoyancyCommon from '../../densityBuoyancyCommon.js';
+import DensityBuoyancyCommonStrings from '../../DensityBuoyancyCommonStrings.js';
+import BuoyancyBasicsExploreModel from '../model/BuoyancyBasicsExploreModel.js';
+import arrayRemove from '../../../../phet-core/js/arrayRemove.js';
+import DensityAccordionBox from '../../buoyancy/view/DensityAccordionBox.js';
+import BuoyancyDisplayOptionsNode from '../../common/view/BuoyancyDisplayOptionsNode.js';
+import SubmergedAccordionBox from '../../buoyancy/view/SubmergedAccordionBox.js';
+import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
+import BlocksRadioButtonGroup from '../../common/view/BlocksRadioButtonGroup.js';
+import BuoyancyExploreScreenView from '../../buoyancy/view/BuoyancyExploreScreenView.js';
+
+// constants
+const MARGIN = DensityBuoyancyCommonConstants.MARGIN;
+
+export default class BuoyancyBasicsExploreScreenView extends DensityBuoyancyScreenView<BuoyancyBasicsExploreModel> {
+
+  protected rightBox: PrimarySecondaryControlsNode;
+
+  public constructor( model: BuoyancyBasicsExploreModel, options: DensityBuoyancyScreenViewOptions ) {
+
+    const tandem = options.tandem;
+
+    super( model, combineOptions<DensityBuoyancyScreenViewOptions>( {
+      cameraLookAt: DensityBuoyancyCommonConstants.BUOYANCY_CAMERA_LOOK_AT
+    }, options ) );
+
+    const buoyancyDisplayOptionsNode = new BuoyancyDisplayOptionsNode( model );
+
+    this.addChild( new AlignBox( new Panel( buoyancyDisplayOptionsNode, DensityBuoyancyCommonConstants.PANEL_OPTIONS ), {
+      alignBoundsProperty: this.visibleBoundsProperty,
+      xAlign: 'left',
+      yAlign: 'bottom',
+      margin: MARGIN
+    } ) );
+
+    const displayedMysteryMaterials = [
+      Material.DENSITY_A,
+      Material.DENSITY_B
+    ];
+
+    const invisibleMaterials = [ ...DensityBuoyancyCommonConstants.BUOYANCY_FLUID_MYSTERY_MATERIALS ];
+    displayedMysteryMaterials.forEach( displayed => arrayRemove( invisibleMaterials, displayed ) );
+
+    this.rightBox = new PrimarySecondaryControlsNode(
+      model.primaryMass,
+      model.secondaryMass,
+      this.popupLayer,
+      {
+        tandem: tandem,
+        minCustomMass: 0.1,
+        supportHiddenMaterial: true,
+        mysteryMaterials: [ Material.MATERIAL_X, Material.MATERIAL_Y ]
+      }
+    );
+
+    const fluidDensityControlPanel = new Panel( new FluidDensityControlNode( model.liquidMaterialProperty, [
+      ...DensityBuoyancyCommonConstants.BUOYANCY_FLUID_MATERIALS,
+      ...DensityBuoyancyCommonConstants.BUOYANCY_FLUID_MYSTERY_MATERIALS
+    ], this.popupLayer, {
+      invisibleMaterials: invisibleMaterials,
+      tandem: tandem.createTandem( 'densityControlNode' )
+    } ), DensityBuoyancyCommonConstants.PANEL_OPTIONS );
+
+    this.addChild( new AlignBox( fluidDensityControlPanel, {
+      alignBoundsProperty: this.visibleBoundsProperty,
+      xAlign: 'center',
+      yAlign: 'bottom',
+      margin: MARGIN
+    } ) );
+
+    [ model.primaryMass, model.secondaryMass ].forEach( mass => {
+      mass.materialProperty.link( material => {
+        if ( material === Material.MATERIAL_X ) {
+          mass.volumeProperty.value = 0.003;
+        }
+        else if ( material === Material.MATERIAL_Y ) {
+          mass.volumeProperty.value = 0.001;
+        }
+      } );
+    } );
+
+    // Materials are set in densityBox.setMaterials() below
+    const densityBox = new DensityAccordionBox( {
+      expandedProperty: model.densityExpandedProperty,
+      contentWidthMax: this.rightBox.content.width
+    } );
+
+    const submergedBox = new SubmergedAccordionBox( model.gravityProperty, model.liquidMaterialProperty, {
+      contentWidthMax: this.rightBox.content.width
+    } );
+
+    const customExploreScreenFormatting = [ model.primaryMass, model.secondaryMass ].map( mass => {
+      return {
+        readoutNameProperty: new PatternStringProperty( DensityBuoyancyCommonStrings.blockPatternStringProperty, { tag: mass.nameProperty } ),
+        readoutFormat: { font: DensityBuoyancyCommonConstants.ITEM_FONT, fill: mass.tag.colorProperty }
+      };
+    } );
+
+    // Adjust the visibility after, since we want to size the box's location for its "full" bounds
+    // This instance lives for the lifetime of the simulation, so we don't need to remove this listener
+    model.secondaryMass.visibleProperty.link( visible => {
+      const masses = visible ? [ model.primaryMass, model.secondaryMass ] : [ model.primaryMass ];
+      densityBox.setReadoutItems( masses.map( ( mass, index ) => {
+        return {
+          readoutItem: mass.materialProperty,
+          readoutNameProperty: customExploreScreenFormatting[ index ].readoutNameProperty,
+          readoutFormat: customExploreScreenFormatting[ index ].readoutFormat
+        };
+      } ) );
+      submergedBox.setReadoutItems( masses.map( ( mass, index ) => {
+        return {
+          readoutItem: mass,
+          readoutNameProperty: customExploreScreenFormatting[ index ].readoutNameProperty,
+          readoutFormat: customExploreScreenFormatting[ index ].readoutFormat
+        };
+      } ) );
+    } );
+
+    const rightSideVBox = new VBox( {
+      spacing: 10,
+      align: 'right',
+      children: [
+        this.rightBox,
+        densityBox,
+        submergedBox
+      ]
+    } );
+
+    this.addChild( new AlignBox( rightSideVBox, {
+      alignBoundsProperty: this.visibleBoundsProperty,
+      xAlign: 'right',
+      yAlign: 'top',
+      margin: MARGIN
+    } ) );
+
+    const blocksRadioButtonGroup = new BlocksRadioButtonGroup( model.modeProperty, {
+      tandem: this.tandem.createTandem( 'blocksRadioButtonGroup' )
+    } );
+
+    ManualConstraint.create( this, [ rightSideVBox, fluidDensityControlPanel, blocksRadioButtonGroup ],
+      ( rightSideVBoxWrapper, fluidDensityControlPanelWrapper, blocksRadioButtonGroupWrapper ) => {
+        blocksRadioButtonGroupWrapper.left = rightSideVBoxWrapper.left;
+        blocksRadioButtonGroupWrapper.bottom = fluidDensityControlPanelWrapper.bottom;
+      } );
+
+    this.addChild( blocksRadioButtonGroup );
+
+    // DerivedProperty doesn't need disposal, since everything here lives for the lifetime of the simulation
+    this.rightBarrierViewPointPropertyProperty.value = new DerivedProperty( [ rightSideVBox.boundsProperty, this.visibleBoundsProperty ], ( boxBounds, visibleBounds ) => {
+      // We might not have a box, see https://github.com/phetsims/density/issues/110
+      return new Vector2( isFinite( boxBounds.left ) ? boxBounds.left : visibleBounds.right, visibleBounds.centerY );
+    }, {
+      strictAxonDependencies: false // This workaround is deemed acceptable for visibleBoundsProperty listening, https://github.com/phetsims/faradays-electromagnetic-lab/issues/65
+    } );
+
+    this.addChild( this.popupLayer );
+  }
+
+  public static getBuoyancyBasicsExploreIcon(): Node {
+    return BuoyancyExploreScreenView.getBuoyancyExploreIcon();
+  }
+}
+
+densityBuoyancyCommon.register( 'BuoyancyBasicsExploreScreenView', BuoyancyBasicsExploreScreenView );

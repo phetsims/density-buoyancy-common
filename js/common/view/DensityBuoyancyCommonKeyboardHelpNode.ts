ebae0a9f02a1ae3d585adff154afd4e0f05ae490
phet-dev <phetadmin@gmail.com>
2024-05-08 03:15:48 -0600

    update copyright dates from daily grunt work

--- js/common/view/DensityBuoyancyCommonKeyboardHelpNode.ts
+++ js/common/view/DensityBuoyancyCommonKeyboardHelpNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * DensityBuoyancyCommonKeyboardHelpNode is the keyboard help for all screens, with parameters that determine whether

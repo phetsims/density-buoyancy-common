1b9ef50d4e3c7a3685af735f07220ea17e5507ab
Sam Reid <reids@colorado.edu>
2024-07-30 09:10:24 -0600

    Add TODO, see https://github.com/phetsims/density-buoyancy-common/issues/123

--- js/buoyancy/view/applications/ApplicationsDebugView.ts
+++ js/buoyancy/view/applications/ApplicationsDebugView.ts
@@ -51,6 +51,7 @@ export default class ApplicationsDebugView extends DebugView {
   public override step( dt: number ): void {
     super.step( dt );
 
+    // TODO: Change this to say this.applicationsModel.boat instead of searches and instanceof operations, see https://github.com/phetsims/density-buoyancy-common/issues/123
     const boat = this.model.masses.find( mass => mass instanceof Boat );
     if ( boat instanceof Boat ) {
       const boatYValues = _.range( boat.stepBottom, boat.stepTop, 0.002 );

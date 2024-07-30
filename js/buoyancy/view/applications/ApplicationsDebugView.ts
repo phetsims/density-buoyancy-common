69dfe168e1fb3350cb3aff9e71f21afc0ecab750
Sam Reid <reids@colorado.edu>
2024-07-30 09:12:04 -0600

    Add TODO, see https://github.com/phetsims/density-buoyancy-common/issues/123

--- js/buoyancy/view/applications/ApplicationsDebugView.ts
+++ js/buoyancy/view/applications/ApplicationsDebugView.ts
@@ -2,6 +2,7 @@
 /**
  * Extends DebugView, which shows a 2d version of the model. In this case, used for the boat's shape.
  * TODO: Document how to show the debug view here. see https://github.com/phetsims/density-buoyancy-common/issues/257
+ * TODO: This file is overly-complicated and under-documented, see https://github.com/phetsims/density-buoyancy-common/issues/123
  *
  * @author Jonathan Olson (PhET Interactive Simulations)
  */

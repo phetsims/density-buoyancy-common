b3feedb55f42e7255239664f58c485a12ea75a2b
Sam Reid <reids@colorado.edu>
2024-08-08 17:20:54 -0600

    Add docs, see https://github.com/phetsims/density-buoyancy-common/issues/317

--- js/common/model/BlendedVector2Property.ts
+++ js/common/model/BlendedVector2Property.ts
@@ -21,9 +21,8 @@ export default class BlendedVector2Property extends Property<Vector2> {
     // choose the blendAmount based on the distance between values
     // This adds a hysteresis effect to the readout, which reduces flickering inherent to the model.
     const MIN_BLEND = 0.1; // When close, blend with the old value more
+    const MAX_BLEND = 0.7; // When far apart, take more of the new value. NOTE: this was manually tuned to be different than BlendedNumberProperty.MAX_BLEND
 
-    // TODO: Does this MAX_BLEND differ from BlendedNumberProperty max on purpose? If so, document. See , see https://github.com/phetsims/density-buoyancy-common/issues/317
-    const MAX_BLEND = 0.7; // When far apart, take the new value completely
     const blendAmount = Utils.clamp(
       Utils.linear( 0, 1, MIN_BLEND, MAX_BLEND, newValue.minus( oldValue ).magnitude ),
       MIN_BLEND, MAX_BLEND );

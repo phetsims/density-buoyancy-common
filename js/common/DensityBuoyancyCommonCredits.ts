0c902b4b220dc0af5b76d6f9a5abf111856a081e
Sam Reid <reids@colorado.edu>
2024-08-13 07:48:56 -0600

    Add credits, see https://github.com/phetsims/density-buoyancy-common/issues/171

---
+++ js/common/DensityBuoyancyCommonCredits.ts
@@ -0,0 +1,21 @@
+// Copyright 2019-2024, University of Colorado Boulder
+
+import { CreditsData } from '../../../joist/js/CreditsNode.js';
+
+/**
+ * Credits for the density/buoyancy sims
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+const DensityBuoyancyCommonCredits: CreditsData = {
+  leadDesign: 'Diana L\u00f3pez Tavares, Kelly Lancaster',
+  softwareDevelopment: 'Michael Kauzmann, Chris Malley (PixelZoom, Inc.), Jonathan Olson, Sam Reid, Agust\u00edn Vallejo',
+  team: 'Wendy Adams, Trish Loeblein, Ariel Paul, Archie Paulson, Kathy Perkins, Noah Podolefsky, Amy Rouinfar, Carl Wieman',
+  qualityAssurance: 'Logan Bray, Steele Dalton, Jaron Droder, Brooklyn Lash, Emily Miller, Liam Mulhall, Devon Quispe, Jacob Romero, Nancy Salpepi, Katie Woessner',
+  graphicArts: '',
+  soundDesign: '',
+  thanks: ''
+};
+
+export default DensityBuoyancyCommonCredits;

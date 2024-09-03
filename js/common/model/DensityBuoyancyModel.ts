2bd19579524580cceae086d1b76735a5a2659c30
AgustinVallejo <agustin.vallejo@udea.edu.co>
2024-09-03 17:56:05 -0500

    Comparing center X and interrupting interaction, see https://github.com/phetsims/density-buoyancy-common/issues/377

--- js/common/model/DensityBuoyancyModel.ts
+++ js/common/model/DensityBuoyancyModel.ts
@@ -239,8 +239,11 @@ export default class DensityBuoyancyModel implements TModel {
             if ( mass !== otherMass ) {
               const horizontalForce = this.engine.bodyGetContactForceBetween( mass.body, otherMass.body ).x;
 
-              // Blocks should never experience +x forces (to the right) by this scale. If they do, they are trapped beneath it.
-              if ( horizontalForce > 0 && mass.getBounds().centerY > otherMass.getBounds().centerY ) {
+              // Blocks should never experience +x forces (to the right) by the PoolScale.
+              // If they do, they are trapped beneath it, so teleport them back to the left.
+              if ( horizontalForce > 0 && mass.getBounds().centerX < otherMass.getBounds().centerX ) {
+
+                otherMass.interruptedEmitter.emit();
 
                 const minSpacing = 0.1; // Ideal new spacing between the scale and the mass
                 const delta = otherMass.getBounds().maxX - mass.getBounds().minX + minSpacing;

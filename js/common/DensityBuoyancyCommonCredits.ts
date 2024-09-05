fd9fab3dbd6b670bc227597d92785a87cfae5e88
Sam Reid <reids@colorado.edu>
2024-09-05 09:11:27 -0600

    Add QA credits, see https://github.com/phetsims/density-buoyancy-common/issues/353

--- js/common/DensityBuoyancyCommonCredits.ts
+++ js/common/DensityBuoyancyCommonCredits.ts
@@ -12,7 +12,7 @@ const DensityBuoyancyCommonCredits: CreditsData = {
   leadDesign: 'Diana L\u00f3pez Tavares, Kelly Lancaster',
   softwareDevelopment: 'Michael Kauzmann, Chris Malley (PixelZoom, Inc.), Jonathan Olson, Sam Reid, Agust\u00edn Vallejo',
   team: 'Wendy Adams, Trish Loeblein, Ariel Paul, Archie Paulson, Kathy Perkins, Noah Podolefsky, Amy Rouinfar, Carl Wieman',
-  qualityAssurance: 'Logan Bray, Steele Dalton, Jaron Droder, Brooklyn Lash, Emily Miller, Matthew Moore, Ashton Morris, Liam Mulhall, Devon Quispe, Jacob Romero, Nancy Salpepi, Katie Woessner',
+  qualityAssurance: 'Logan Bray, Steele Dalton, Jaron Droder, Clifford Hardin, Brooklyn Lash, Emily Miller, Matthew Moore, Ashton Morris, Liam Mulhall, Devon Quispe, Jacob Romero, Nancy Salpepi, Katie Woessner',
   graphicArts: '',
   soundDesign: '',
   thanks: ''

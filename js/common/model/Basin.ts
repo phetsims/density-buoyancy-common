290c769f8c6d2a17c087477aa478599d8cf115a7
phet-dev <phetadmin@gmail.com>
2025-02-14 03:28:39 -0700

    update copyright dates from daily grunt work

--- js/common/model/Basin.ts
+++ js/common/model/Basin.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * Represents a basin that a fluid can reside in at a specific level. This is used for the pool and fluid in the boat.

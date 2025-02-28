8139f9c36b1feed5284f0bb2570d12fcb370a447
phet-dev <phetadmin@gmail.com>
2025-02-28 03:30:09 -0700

    update copyright dates from daily grunt work

--- js/buoyancy/model/shapes/BuoyancyShapeModel.ts
+++ js/buoyancy/model/shapes/BuoyancyShapeModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * The main model for a single shape model object in the sim. This manages changing the shape, phet-io, and updating

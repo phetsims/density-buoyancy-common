8e1ec047a356bc8eb5947abc6fa65d42433bde54
phet-dev <phetadmin@gmail.com>
2025-04-03 03:18:07 -0600

    update copyright dates from daily grunt work

--- js/common/model/InterpolatedProperty.ts
+++ js/common/model/InterpolatedProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2024, University of Colorado Boulder
+// Copyright 2019-2025, University of Colorado Boulder
 
 /**
  * A Property that is based on the step-based interpolation between a current (most recent value) and previous value

92f90867180b0a423b840528c303de639e3c604f
phet-dev <phetadmin@gmail.com>
2025-06-26 03:27:37 -0600

    update copyright dates from daily grunt work

--- js/common/model/BlockSet.ts
+++ js/common/model/BlockSet.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * Determines which variable in the density equation is consistent across all blocks in the scene, see BlockSetModel.
